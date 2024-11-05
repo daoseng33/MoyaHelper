dc82450b2717b55f1a313f2e6cfb263ececf82c9
DAO <daoseng33@gmail.com>
2024-11-05 13:47:50 +0800

    chore: minmum support iOS 16

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "MoyaHelper",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
         .library(
             name: "MoyaHelper",
